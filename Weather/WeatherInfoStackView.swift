dde54c9a80775bd0b6011e096b60b3bec1f0f82f
nhandoantrong <nhandoantrong17498@gmail.com>
2021-12-16 16:10:35 +0700

    First commit

---
+++ Weather/WeatherInfoStackView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherInfoStackView.swift
+//  Weather
+//
+//  Created by Nhan Doan on 15/12/2021.
+//
+
+import UIKit
+
+class WeatherInfoStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
